fe543d476218f2b44fde506deb2d3e1f450e6765
f-i-t-s-u-m <fibabirr@gmail.com>
2022-07-01 11:04:41 +0300

    add task 1

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,11 @@
+-- Write a SQL script that creates a table users following these requirements:
+-- with these attributes
+-- id, email, name, country
+
+CREATE TABLE IF NOT EXISTS `users` (
+	`id` int(11) AUTO_INCREMENT,
+	`email` varchar(255) not null unique,
+	`name` varchar(255),
+	`country` enum('US', 'CO', 'TN') default('USI'),
+	PRIMARY KEY(`id`)
+	)
