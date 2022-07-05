edde45112366e899a386ea24f47ff0fa830d2877
f-i-t-s-u-m <fibabirr@gmail.com>
2022-07-05 12:33:44 +0300

    add task 8

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Write a Python function that lists all documents in a collection:
+
+    Prototype: def list_all(mongo_collection):
+    Return an empty list if no document in the collection
+    mongo_collection will be the pymongo collection object
+    
+"""
+
+
+def list_all(mongo_collection):
+    """ list all collection """
+    return mongo_collection.find()
