b3d802c39da3f45f6b5299a7479f957543ef2a0e
f-i-t-s-u-m <fibabirr@gmail.com>
2022-07-06 13:34:15 +0300

    add task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -9,13 +9,30 @@ from uuid import uuid4
 class Cache():
     """ cache class """
     _redis = Redis()
+
     def __init__(self):
         """ init the cache instance """
         self._redis.flushdb()
 
-    def store(self, data) -> str :
+    def store(self, data: bytes) -> str:
         """ store data to redis """
         id = str(uuid4())
         self._redis.set(id, data)
         return id
 
+    def get(self, key: str, fn=None):
+        """ get data """
+        data = self._redis.get(key)
+        if (fn is not None):
+            if(isinstance(fn, (str, int))):
+                return f'get_{fn}'(data)
+            return fn(data)
+        return data
+
+    def get_str(self, data):
+        """ convert to str """
+        return data.decode('utf-8')
+
+    def get_int(self, data):
+        """ convert to int """
+        return int(data)
