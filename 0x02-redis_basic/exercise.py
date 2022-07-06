21b6dd37fa5c178e507aa099a838fa7638f39446
f-i-t-s-u-m <fibabirr@gmail.com>
2022-07-06 12:12:57 +0300

    init project with task 0

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" python3 with redis db """
+
+
+from redis import Redis
+from uuid import uuid4
+
+
+class Cache():
+    """ cache class """
+    _redis = Redis()
+    def __init__(self):
+        """ init the cache instance """
+        self._redis.flushdb()
+
+    def store(self, data) -> str :
+        """ store data to redis """
+        id = str(uuid4())
+        self._redis.set(id, str(data))
+        return id
+
