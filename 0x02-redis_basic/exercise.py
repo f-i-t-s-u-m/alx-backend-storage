0586ca2c320a3b356f4ad689d8ef1a1ceb0f1d79
f-i-t-s-u-m <fibabirr@gmail.com>
2022-07-06 22:59:50 +0300

    add task 2

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,6 +4,8 @@
 
 from redis import Redis
 from uuid import uuid4
+from functools import wraps
+from typing import Callable, List, Union, Optional
 
 
 class Cache():
@@ -14,25 +16,38 @@ class Cache():
         """ init the cache instance """
         self._redis.flushdb()
 
-    def store(self, data: bytes) -> str:
+    def count_calls(method: Callable) -> Callable:
+        """ decorator for method """
+        @wraps(method)
+        def wrapper(self, *args, **kwargs):
+            """ wrapper for the decorator """
+            self._redis.incr(method.__qualname__)
+            return method(self, *args, **kwargs)
+        return wrapper
+
+    @count_calls
+    def store(self, data: Union[str, bytes, int, List]) -> str:
         """ store data to redis """
         id = str(uuid4())
         self._redis.set(id, data)
         return id
 
-    def get(self, key: str, fn=None):
+    @count_calls
+    def get(self, key: str, fn: Optional[Callable] = None) -> str:
         """ get data """
         data = self._redis.get(key)
         if (fn is not None):
             if(isinstance(fn, (str, int))):
-                return f'get_{fn}'(data)
+                return eval(f'get_{fn.__name__}({data})')
             return fn(data)
         return data
 
-    def get_str(self, data):
+    @count_calls
+    def get_str(self, data: bytes) -> str:
         """ convert to str """
         return data.decode('utf-8')
 
-    def get_int(self, data):
+    @count_calls
+    def get_int(self, data: bytes) -> int:
         """ convert to int """
         return int(data)
